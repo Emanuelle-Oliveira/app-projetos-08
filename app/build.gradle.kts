395c6c49f12bf36f5660b25af618861e00736e18
Emanuelle Oliveira <emanuelletrindadeoliveira@gmail.com>
2023-10-28 17:51:38 -0300

    feat: examples

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ android {
 }
 
 dependencies {
-
   implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
   implementation("androidx.activity:activity-compose:1.7.0")
@@ -60,6 +59,7 @@ dependencies {
   implementation("androidx.compose.material3:material3")
   implementation("com.google.firebase:firebase-database:20.3.0")
   implementation("com.google.firebase:firebase-firestore:24.9.1")
+  implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/appprojetos08/MainActivity.kt
+++ app/src/main/java/com/example/appprojetos08/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appprojetos08
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,29 +9,64 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.appprojetos08.models.output.Output
+import com.example.appprojetos08.models.sensor.Sensor
+import com.example.appprojetos08.services.output.OutputService
 import com.example.appprojetos08.ui.theme.AppProjetos08Theme
 import com.google.firebase.Firebase
-import com.google.firebase.database.database
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
+  /*var outputList = mutableListOf<Output>()
+
+  private fun getOutputs() {
+    lifecycleScope.launch {
+      outputList = OutputService().getAll()
+    }
+  }
+
+  private fun getOutputsByGroupId(id: Int) {
+    lifecycleScope.launch {
+      outputList = OutputService().getByGroupId(id)
+    }
+  }*/
+
+   /*
+   TESTES DAS FUNÇÔES PARA EXEMPLO
+
+   OUTPUT:
+   - get all
+   lifecycleScope.launch {
+     val outputList = OutputService().getAll()
+     Log.d("Log", "Lista de saídas: $outputList")
+   }
+   - getByGroupId
+   lifecycleScope.launch {
+     val outputList = OutputService().getByGroupId(0)
+     Log.d("Log", "Lista de saídas: $outputList")
+   }
+   */
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    //getOutputs()
+    //getOutputsByGroupId(1)
+
     setContent {
       AppProjetos08Theme {
-        // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
           Greeting("Android")
         }
       }
     }
-
-    // Write a message to the database
-    val database = Firebase.database
-    val myRef = database.getReference("message")
-
-    myRef.setValue("Hello, World!")
   }
 }
 
